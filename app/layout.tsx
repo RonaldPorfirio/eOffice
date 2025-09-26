0c7e23f811c5cf219adb0109df1be19b7af7f26d
Ronald Porfirio <ronaldbolsadevalor@outlook.com>
2025-09-26 08:26:05 -0300

    eOffice_fix9

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'E-Office 1321',
+  description: 'Um Escritório Virtual é ideal para você, profissional liberal, MEI ou empreendedor que deseja ter um endereço comercial, mas não quer arcar com os custos de um escritório físico.',
+  generator: 'Ronald Porfirio',
 }
 
 export default function RootLayout({
